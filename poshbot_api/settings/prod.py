211d50e70b51127ad361fde6784105351ba529c0
Edwin Cruz <ecruz1113@gmail.com>
2022-04-21 15:28:09 -0400

    Add database config for prod

--- poshbot_api/settings/prod.py
+++ poshbot_api/settings/prod.py
@@ -6,3 +6,15 @@ DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": os.environ['SQL_DATABASE'],
+        "USER": os.environ['SQL_USER'],
+        "PASSWORD": os.environ['SQL_PASSWORD'],
+        "HOST": os.environ['SQL_HOST'],
+        "PORT": os.environ['SQL_PORT'],
+    }
+}
+
