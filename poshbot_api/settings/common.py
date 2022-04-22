c617e39582ed96d694909b78daae62ece93dfe6a
Edwin Cruz <ecruz1113@gmail.com>
2022-04-22 10:08:52 -0400

    Add storages to the installed apps

--- poshbot_api/settings/common.py
+++ poshbot_api/settings/common.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,6 +27,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'debug_toolbar',
     'django_filters',
+    'storages',
     'djoser',
     'corsheaders',
     'core'
