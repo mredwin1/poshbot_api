5599f0060a05a437aecce18b16967c94458da6e4
Edwin Cruz <ecruz1113@gmail.com>
2022-04-21 23:23:07 -0400

    Add the right host to allowed hosts

--- poshbot_api/settings/prod.py
+++ poshbot_api/settings/prod.py
@@ -6,7 +6,7 @@ DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = [
-    'http://poshbot-api-prod.us-east-1.elasticbeanstalk.com/'
+    'poshbot-api-prod.us-east-1.elasticbeanstalk.com'
 ]
 
 DATABASES = {
