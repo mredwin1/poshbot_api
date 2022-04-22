524ef296c16fdd60f90c1ef1d8cc45db81c189a0
Edwin Cruz <ecruz1113@gmail.com>
2022-04-21 22:03:15 -0400

    Remove unecessary comments

--- poshbot_api/settings/dev.py
+++ poshbot_api/settings/dev.py
@@ -3,6 +3,3 @@ from .common import *
 DEBUG = True
 
 SECRET_KEY = 'django-insecure-s(o=%hz@0mm%bt@wfi6&1tzj$q)gom4kfe$ll_#4i0%17hoaxt'
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
