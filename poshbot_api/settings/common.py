6bf971c3e82bb1be8335c450007deccbf31056ac
Edwin Cruz <ecruz1113@gmail.com>
2022-04-23 01:04:13 -0400

    Remove file logger

--- poshbot_api/settings/common.py
+++ poshbot_api/settings/common.py
@@ -140,15 +140,15 @@ LOGGING = {
         'console': {
             'class': 'logging.StreamHandler',
         },
-        'file': {
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/app-logs/general.log',
-            'formatter': 'verbose'
-        },
+        # 'file': {
+        #     'class': 'logging.FileHandler',
+        #     'filename': '/var/log/app-logs/general.log',
+        #     'formatter': 'verbose'
+        # },
     },
     'loggers': {
         '': {
-            'handlers': ['console', 'file'],
+            'handlers': ['console'],
             'level': os.environ.get('DJANGO_LOG_LEVEL', 'INFO'),
         }
     },
