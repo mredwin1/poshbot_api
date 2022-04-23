272d0a9590c4de1d35f6d0e1d741e7564167c4f5
Edwin Cruz <ecruz1113@gmail.com>
2022-04-23 12:37:38 -0400

    Migrate database on start

--- core/management/commands/start.py
+++ core/management/commands/start.py
@@ -1,8 +1,11 @@
 import logging
 import os
+import sys
+import time
 
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
 from core.models import User, Campaign
 
 
@@ -10,6 +13,24 @@ class Command(BaseCommand):
     help = 'An alternative to runserver which will run migrate and collectstatic beforehand'
 
     def handle(self, *args, **options):
+        attempts_left = 5
+        while attempts_left:
+            try:
+                logging.info('Trying to run migrations...')
+                call_command("migrate")
+                logging.info('Migrations complete')
+                break
+            except OperationalError as error:
+                if error.args[0] == 'FATAL:  the database system is starting up\n':
+                    attempts_left -= 1
+                    logging.warning('Cannot run migrations because the database system is starting up, retrying.')
+                    time.sleep(0.5)
+                else:
+                    sys.exit(f'Migrations unsuccessful. Error: {error.args}')
+        else:
+            logging.error('Migrations could not be run, exiting.')
+            sys.exit('Migrations unsuccessful')
+
         superusername = os.environ.get('SUPER_USERNAME')
         if superusername:
             if not User.objects.filter(username=superusername).exists():
