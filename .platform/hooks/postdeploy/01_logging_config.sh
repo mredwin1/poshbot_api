02f87aa56ffa85b4417b23915431426b231325ab
Edwin Cruz <ecruz1113@gmail.com>
2022-04-23 03:13:08 -0400

    Change hook scripts

--- .platform/hooks/postdeploy/01_logging_config.sh
+++ .platform/hooks/postdeploy/01_logging_config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mkdir /var/log/app_logs
-chmod g+s /var/log/app_logs
-setfacl -d -m g::rw /var/log/app_logs
-chown wsgi:wsgi /var/log/app_logs
+#mkdir /var/log/app_logs
+#chmod g+s /var/log/app_logs
+#setfacl -d -m g::rw /var/log/app_logs
+#chown wsgi:wsgi /var/log/app_logs
+echo Hello
--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source /var/app/venv/*/bin/activate
-cd /var/app/current
-
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
+#cd /var/app/current
+#
+#python manage.py makemigrations
+#python manage.py migrate
+#python manage.py collectstatic --noinput
