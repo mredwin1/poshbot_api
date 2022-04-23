a92ee25a4e9fbee87c7b1f1af12526e3770fcf72
Edwin Cruz <ecruz1113@gmail.com>
2022-04-23 16:43:20 -0400

    Change default access token lifetime

--- poshbot_api/settings/common.py
+++ poshbot_api/settings/common.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+from datetime import timedelta
 import os
 from pathlib import Path
 
@@ -125,6 +126,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2)
 }
 
 DJOSER = {
