1f86e3cbe6a0a0b72342e52e502fc871a4c58590
Edwin Cruz <ecruz1113@gmail.com>
2022-04-23 03:01:59 -0400

    Remove unecessary command

--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -3,6 +3,11 @@
 source /var/app/venv/*/bin/activate
 cd /var/app/staging
 
+mkdir -p /var/log/app-logs
+chmod g+s /var/log/app-logs
+setfacl -d -m g::rw /var/log/app-logs
+chown wsgi:wsgi /var/log/app-logs
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
