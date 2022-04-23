d3a1ca60dcb384f1e328e84592a5a6ec0103e105
Edwin Cruz <ecruz1113@gmail.com>
2022-04-23 03:34:34 -0400

    Change directory target for migrations

--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-source /var/app/venv/*/bin/activate
-cd /var/app/staging
-
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
+echo Hello
+#source /var/app/venv/*/bin/activate
+#cd /var/app/staging
+#
+#python manage.py makemigrations
+#python manage.py migrate
+#python manage.py collectstatic --noinput
