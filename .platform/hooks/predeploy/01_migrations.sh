6b41990b7875ebf6cb6258788f74a8882e400d37
Edwin Cruz <ecruz1113@gmail.com>
2022-04-23 03:38:23 -0400

    Remove .platform

--- .platform/hooks/predeploy/01_migrations.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo Hello
-#source /var/app/venv/*/bin/activate
-#cd /var/app/staging
-#
-#python manage.py makemigrations
-#python manage.py migrate
-#python manage.py collectstatic --noinput
