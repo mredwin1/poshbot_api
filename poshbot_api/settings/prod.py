5e03558e1588372799c293e6325efa7e71a527e0
Edwin Cruz <ecruz1113@gmail.com>
2022-04-23 00:26:34 -0400

    Remove unnecessary del

--- poshbot_api/settings/prod.py
+++ poshbot_api/settings/prod.py
@@ -44,5 +44,3 @@ AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
 
 AWS_S3_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-
-del socket
