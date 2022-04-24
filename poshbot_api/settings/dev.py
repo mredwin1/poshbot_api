1955a884361c62d7fbff376f6c8e82be042459c7
Edwin Cruz <ecruz1113@gmail.com>
2022-04-24 03:04:57 -0400

    Set up dev environment again

--- poshbot_api/settings/dev.py
+++ poshbot_api/settings/dev.py
@@ -2,6 +2,25 @@ from .common import *
 
 DEBUG = True
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://localhost'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
 SECRET_KEY = 'django-insecure-s(o=%hz@0mm%bt@wfi6&1tzj$q)gom4kfe$ll_#4i0%17hoaxt'
 
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'].append('rest_framework.renderers.BrowsableAPIRenderer')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'poshbot',
+        'USER': 'postgres',
+        'PASSWORD': 'Apples2Apples!',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
