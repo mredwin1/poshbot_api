ff853f16e6c3cca812f0923c07acf1074d2039be
Edwin Cruz <ecruz1113@gmail.com>
2022-04-26 00:41:29 -0400

    Add pagination to all views

--- poshbot_api/settings/common.py
+++ poshbot_api/settings/common.py
@@ -121,7 +121,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 1000
 }
 
 SIMPLE_JWT = {
