e27f3726754ecb37d89850ab1f0374ed51ef4aea
Edwin Cruz <ecruz1113@gmail.com>
2022-05-01 14:21:26 -0400

    Stop campaign from running if it has status stopped

--- core/tasks.py
+++ core/tasks.py
@@ -11,14 +11,15 @@ logger = logging.getLogger(__name__)
 def advanced_sharing_campaign(campaign_id):
     print(f'Running Advanced Sharing campaign (Campaign ID: {campaign_id})')
     campaign = Campaign.objects.get(id=campaign_id)
-    campaign.status = Campaign.RUNNING
-    campaign.save()
-
-    with PoshMarkClient(campaign, logger) as client:
-        client.register()
-
     if campaign.status != Campaign.STOPPED:
-        campaign.status = Campaign.IDLE
+        campaign.status = Campaign.RUNNING
         campaign.save()
-        advanced_sharing_campaign.apply_async(countdown=5, kwargs={'campaign_id': campaign_id})
+
+        with PoshMarkClient(campaign, logger) as client:
+            client.register()
+
+        if campaign.status != Campaign.STOPPED:
+            campaign.status = Campaign.IDLE
+            campaign.save()
+            advanced_sharing_campaign.apply_async(countdown=5, kwargs={'campaign_id': campaign_id})
     print('Campaign ended')
