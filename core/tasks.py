a30a97536744031bdfcbdcb8f6eb3fd12002e97b
Edwin Cruz <ecruz1113@gmail.com>
2022-05-09 15:24:16 -0400

    Change proxy IP

--- core/tasks.py
+++ core/tasks.py
@@ -25,7 +25,7 @@ def advanced_sharing_campaign(campaign_id):
 
         start_time = time.time()
 
-        with PoshMarkClient(campaign, logger, '192.154.249.111', '8000') as client:
+        with PoshMarkClient(campaign, logger, '192.154.246.203', '8000') as client:
             client.check_ip()
             while not campaign.posh_user.is_registered and not campaign.posh_user.profile_updated and register_retries < 3:
                 client.register()
@@ -65,6 +65,42 @@ def advanced_sharing_campaign(campaign_id):
             hours, remainder = divmod(campaign_delay, 3600)
             minutes, seconds = divmod(remainder, 60)
             logger.info(f'Campaign will start back up in {round(hours)} hours {round(minutes)} minutes and {round(seconds)} seconds')
+            advanced_sharing_campaign.apply_async(countdown=campaign_delay, kwargs={'campaign_id': campaign_id})
+
+    print('Campaign ended')
+
+
+@shared_task
+def basic_sharing_campaign(campaign_id):
+    print(f'Running Basic Sharing campaign (Campaign ID: {campaign_id})')
+    campaign = Campaign.objects.get(id=campaign_id)
+    delay = campaign.delay * 60
+    deviation = random.randint(0, (delay / 2))
+
+    if campaign.status != Campaign.STOPPED and campaign.posh_user.is_active:
+        campaign.status = Campaign.RUNNING
+        campaign.save()
+
+        start_time = time.time()
+
+        if campaign.posh_user.is_registered:
+            with PoshMarkClient(campaign, logger) as client:
+                all_listings = client.get_all_listings()
+                for shareable_listing in all_listings['shareable_listings']:
+                    client.share_item(shareable_listing)
+
+        end_time = time.time()
+        elapsed_time = round(end_time - start_time, 2)
+        campaign_delay = (delay - elapsed_time) - deviation
+
+        campaign.refresh_from_db()
+        if campaign.status != Campaign.STOPPED:
+            campaign.status = Campaign.IDLE
+            campaign.save()
+            hours, remainder = divmod(campaign_delay, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            logger.info(
+                f'Campaign will start back up in {round(hours)} hours {round(minutes)} minutes and {round(seconds)} seconds')
             advanced_sharing_campaign.apply_async(countdown=campaign_delay, kwargs={'campaign_id': campaign_id})
 
     print('Campaign ended')
