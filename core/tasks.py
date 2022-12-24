ff7647093820621beaaaf3cfe4a6d60af18bbca1
Edwin Cruz <ecruz1113@gmail.com>
2022-12-23 23:24:01 -0500

    remove check ip

--- core/tasks.py
+++ core/tasks.py
@@ -28,7 +28,6 @@ def advanced_sharing_campaign(campaign_id):
         start_time = time.time()
 
         with PoshMarkClient(campaign, logger, proxy_hostname='gate.smartproxy.com', proxy_port='10000') as client:
-            client.check_ip()
             while not campaign.posh_user.is_registered and not campaign.posh_user.profile_updated and register_retries < 3:
                 client.register()
                 client.update_profile()
