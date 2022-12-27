871b4e3b21489f33b8ca14e2c57b662ece45f333
Edwin Cruz <ecruz1113@gmail.com>
2022-12-27 14:49:16 -0500

    Stop campaign if share was unsuccessful

--- core/tasks.py
+++ core/tasks.py
@@ -42,6 +42,7 @@ def advanced_sharing_campaign(campaign_id):
                 all_listing_titles = []
 
                 if all_listings:
+                    shared = False
                     for listings in all_listings.values():
                         all_listing_titles += listings
 
@@ -50,25 +51,37 @@ def advanced_sharing_campaign(campaign_id):
                             listing_images = ListingImage.objects.filter(listing=listing)
                             client.list_item(listing, listing_images)
                         elif listing.title in all_listings['shareable_listings']:
-                            client.share_item(listing.title)
+                            listing_shared = client.share_item(listing.title)
 
-                            today = datetime.datetime.today()
-                            nine_pm = datetime.datetime(year=today.year, month=today.month, day=(today.day + 1), hour=2, minute=0,
-                                                        second=0)
-                            if today > nine_pm:
-                                client.send_offer_to_likers(listing.title)
+                            if not shared:
+                                shared = listing_shared
 
-                            client.check_offers(listing.title)
+                    if random.random() < .10 and shared:
+                        today = datetime.datetime.today()
+                        nine_pm = datetime.datetime(year=today.year, month=today.month, day=(today.day + 1),
+                                                    hour=2, minute=0, second=0)
+                        if today > nine_pm:
+                            client.send_offer_to_likers(listing.title)
 
-        response = requests.get('https://portal.mobilehop.com/proxies/b6e8b8a1f38f4ba3937aa83f6758903a/reset')
-        logger.info(response.text)
-        time.sleep(15)
-        end_time = time.time()
-        elapsed_time = round(end_time - start_time, 2)
-        campaign_delay = (delay - elapsed_time) + deviation if elapsed_time > 1 else deviation
+                    if random.random() < .20 and shared:
+                        client.check_offers(listing.title)
+
+                    if not shared:
+                        campaign.status = Campaign.STOPPED
+                        campaign.save()
+                else:
+                    campaign.status = Campaign.STOPPED
+                    campaign.save()
 
         campaign.refresh_from_db()
         if campaign.status != Campaign.STOPPED:
+            response = requests.get('https://portal.mobilehop.com/proxies/b6e8b8a1f38f4ba3937aa83f6758903a/reset')
+            logger.info(response.text)
+            time.sleep(15)
+            end_time = time.time()
+            elapsed_time = round(end_time - start_time, 2)
+            campaign_delay = (delay - elapsed_time) + deviation if elapsed_time > 1 else deviation
+
             campaign.status = Campaign.IDLE
             campaign.save()
             hours, remainder = divmod(campaign_delay, 3600)
