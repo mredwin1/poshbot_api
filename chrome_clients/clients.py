700f638ca6c08466a814a91dcbc81c551d2b2863
Edwin Cruz <ecruz1113@gmail.com>
2022-12-27 16:44:50 -0500

    Extend allowed elapsed_time to 60sec

--- chrome_clients/clients.py
+++ chrome_clients/clients.py
@@ -600,7 +600,7 @@ class PoshMarkClient(BaseClient):
                             elif unit == 'hours':
                                 elapsed_time = int(timestamp[:space_index]) * 60 * 60
 
-                        if elapsed_time > 25:
+                        if elapsed_time > 60:
                             self.logger.error(f'Sharing does not seem to be working '
                                               f'Elapsed Time: {elapsed_time} {unit}')
                             return False
