a55709befeac95bc9ecf6b0760101e000e447790
Edwin Cruz <ecruz1113@gmail.com>
2022-12-28 21:52:28 -0500

    Fix task names

--- poshbot_api/settings/common.py
+++ poshbot_api/settings/common.py
@@ -169,7 +169,7 @@ CELERY_BROKER_URL = f'redis://redis:6379/1'
 CELERY_RESULT_BACKEND = f'redis://redis:6379/1'
 
 CELERY_TASK_ROUTES = {
-    'core.tasks.basic_sharing': {'queue': 'concurrency', 'routing_key': 'concurrency'},
-    'core.tasks.advanced_sharing': {'queue': 'concurrency', 'routing_key': 'concurrency'},
+    'core.tasks.basic_sharing_campaign': {'queue': 'concurrency', 'routing_key': 'concurrency'},
+    'core.tasks.advanced_sharing_campaign': {'queue': 'concurrency', 'routing_key': 'concurrency'},
     'core.tasks.init_campaign': {'queue': 'no_concurrency', 'routing_key': 'no_concurrency'},
 }
