546d4335b265eebfe7cb33b137b934b7e6c6f5c2
Edwin Cruz <ecruz1113@gmail.com>
2023-01-02 18:43:11 -0500

    Add post delete signals for Listings, Logs
    This post delete signal is to handle cleaning up
    all of the s3 files

--- core/signals.py
+++ core/signals.py
@@ -3,7 +3,7 @@ import os
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from core.models import PoshUser
+from core.models import PoshUser, Listing, ListingImage, LogEntry
 
 
 @receiver(post_delete, sender=PoshUser)
@@ -17,3 +17,21 @@ def posh_user_deleted(sender, instance, *args, **kwargs):
         os.remove(f'/shared_volume/cookies/{instance.username}.pkl')
     except OSError:
         pass
+
+    instance.profile_picture.delete(save=False)
+    instance.header_picture.delete(save=False)
+
+
+@receiver(post_delete, sender=Listing)
+def listing_deleted(sender, instance, *args, **kwargs):
+    instance.cover_photo.delete(save=False)
+
+
+@receiver(post_delete, sender=ListingImage)
+def listing_image_deleted(sender, instance, *args, **kwargs):
+    instance.image.delete(save=False)
+
+
+@receiver(post_delete, sender=LogEntry)
+def log_entry_deleted(sender, instance, *args, **kwargs):
+    instance.image.delete(save=False)
