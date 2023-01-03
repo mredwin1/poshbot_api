5eb13d823006a642d2bc47ea08bd735fcd1a9978
Edwin Cruz <ecruz1113@gmail.com>
2023-01-03 03:53:44 -0500

    Change

--- chrome_clients/clients.py
+++ chrome_clients/clients.py
@@ -251,13 +251,13 @@ class PoshMarkClient(BaseClient):
         self.posh_user = campaign.posh_user
         self.campaign = campaign
         self.requests_proxy = {}
+
         if proxy_hostname and proxy_port:
             self.requests_proxy['https'] = f'http://{proxy_hostname}:{proxy_port}'
-        self.create_error_dir()
 
-    def create_error_dir(self):
         if not os.path.isdir(f'/log_images/{self.campaign.title}'):
-            os.mkdir('log_images')
+            if not os.path.isdir('/log_images'):
+                os.mkdir('log_images')
             os.mkdir(f'/log_images/{self.campaign.title}')
 
     def handle_error(self, error_message, filename):
