77ab78de0c6ee7ca0439acf3f0b9606ff2645154
Edwin Cruz <ecruz1113@gmail.com>
2023-01-05 12:54:28 -0500

    Remove email delete on posh user delete

--- core/signals.py
+++ core/signals.py
@@ -9,11 +9,6 @@ from core.models import PoshUser, Listing, ListingImage, LogEntry
 @receiver(post_delete, sender=PoshUser)
 def posh_user_deleted(sender, instance, *args, **kwargs):
     try:
-        instance.delete_email()
-    except:
-        pass
-
-    try:
         os.remove(f'/shared_volume/cookies/{instance.username}.pkl')
     except OSError:
         pass
