229cbd4112b6ab117d3b0959b081872a1f0cc5ff
Edwin Cruz <ecruz1113@gmail.com>
2023-01-06 09:49:27 -0500

    Add some more error handling

--- core/tasks.py
+++ core/tasks.py
@@ -8,7 +8,7 @@ import time
 import traceback
 
 from celery import shared_task
-from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import WebDriverException, SessionNotCreatedException
 
 from chrome_clients.clients import PoshMarkClient
 from .models import Campaign, Listing, ListingImage, ProxyConnection, LogGroup
@@ -223,11 +223,11 @@ def advanced_sharing_campaign(campaign_id, logger_id=None, proxy_hostname=None, 
                     minutes, seconds = divmod(remainder, 60)
                     logger.info(f'Campaign will start back up in {round(hours)} hours {round(minutes)} minutes and {round(seconds)} seconds')
                     advanced_sharing_campaign.apply_async(countdown=campaign_delay, kwargs={'campaign_id': campaign_id})
-            except WebDriverException as e:
+            except (WebDriverException, SessionNotCreatedException) as e:
                 logger.error(f'{traceback.format_exc()}')
                 proxy_connection = ProxyConnection.objects.get(campaign=campaign)
                 proxy_connection.hard_rest()
-                time.sleep(180)
+                advanced_sharing_campaign.apply_async(countdown=delay + 180, kwargs={'campaign_id': campaign_id})
 
         if not campaign.posh_user.is_active:
             campaign.status = Campaign.STOPPED
@@ -318,8 +318,9 @@ def basic_sharing_campaign(campaign_id, logger_id=None):
                     minutes, seconds = divmod(remainder, 60)
                     logger.info(f'Campaign will start back up in {round(hours)} hours {round(minutes)} minutes and {round(seconds)} seconds')
                     basic_sharing_campaign.apply_async(countdown=campaign_delay, kwargs={'campaign_id': campaign_id})
-            except WebDriverException as e:
+            except (WebDriverException, SessionNotCreatedException) as e:
                 logger.error(f'{traceback.format_exc()}')
+                advanced_sharing_campaign.apply_async(countdown=delay + 180, kwargs={'campaign_id': campaign_id})
 
         if not campaign.posh_user.is_active or not campaign.posh_user.is_registered:
             campaign.status = Campaign.STOPPED
