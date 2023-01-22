259e04075e71a66dcdbc5ee5da7ddb4c9b872e9b
Edwin Cruz <ecruz1113@gmail.com>
2023-01-21 21:55:28 -0500

    Increase retry count

--- appium_clients/clients.py
+++ appium_clients/clients.py
@@ -129,7 +129,7 @@ class AppiumClient:
         self.download_and_send_file(profile_picture_key, campaign_folder)
 
         retries = 0
-        while not self.locate(AppiumBy.ID, 'com.poshmark.app:id/sign_up_option') and retries < 5:
+        while not self.locate(AppiumBy.ID, 'com.poshmark.app:id/sign_up_option') and retries < 10:
             self.sleep(7)
             retries += 1
 
