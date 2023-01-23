e141753e4fdd447ea6146eb07b38ecfa585d1217
Edwin Cruz <ecruz1113@gmail.com>
2023-01-23 11:38:10 -0500

    Get category button again

--- appium_clients/clients.py
+++ appium_clients/clients.py
@@ -329,6 +329,7 @@ class AppiumClient:
 
         self.sleep(1)
 
+        secondary_category_button = self.locate(AppiumBy.ACCESSIBILITY_ID, secondary_category.lower())
         action = ActionChains(self.driver).click_and_hold(on_element=secondary_category_button).move_by_offset(xoffset=0, yoffset=-100)
         action.perform()
 
