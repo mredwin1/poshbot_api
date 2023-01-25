ea91061c94bf15d794124312274c1998230c67c1
Edwin Cruz <ecruz1113@gmail.com>
2023-01-25 08:54:46 -0500

    Have scroll also be based on based on media

--- appium_clients/clients.py
+++ appium_clients/clients.py
@@ -342,8 +342,9 @@ class AppiumClient:
 
         self.sleep(1)
 
+        media_items = self.locate(AppiumBy.ID, 'com.poshmark.app:id/media_items')
         description_input = self.locate(AppiumBy.ID, 'com.poshmark.app:id/description_body')
-        action = ActionChains(self.driver).click_and_hold(on_element=description_input).move_by_offset(xoffset=0, yoffset=(-1658 - description_input.size['height']))
+        action = ActionChains(self.driver).click_and_hold(on_element=description_input).move_by_offset(xoffset=0, yoffset=(-1383 - media_items.size['height'] - description_input.size['height']))
         action.perform()
 
         listing_category = listing.category
