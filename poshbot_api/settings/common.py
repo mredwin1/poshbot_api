331092de42043fd931dc36eaeddb822c4e86931d
Edwin Cruz <ecruz1113@gmail.com>
2023-02-04 21:07:05 -0500

    Add route for check_posh_users task

--- poshbot_api/settings/common.py
+++ poshbot_api/settings/common.py
@@ -179,6 +179,7 @@ CELERY_TASK_ROUTES = {
     'core.tasks.init_campaign': {'queue': 'campaign_no_concurrency.fifo', 'routing_key': 'no_concurrency'},
     'core.tasks.CampaignTask': {'queue': 'campaign_concurrency', 'routing_key': 'campaign_concurrency'},
     'core.tasks.restart_campaigns': {'queue': 'maintenance', 'routing_key': 'maintenance'},
+    'core.tasks.check_posh_users': {'queue': 'maintenance', 'routing_key': 'maintenance'},
 }
 
 CELERY_BEAT_SCHEDULE = {
