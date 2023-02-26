bdd6bfd34b527b249d18460db84402770fd4ad40
Edwin Cruz <ecruz1113@gmail.com>
2023-02-26 18:43:17 -0500

    Set datetime removed

--- core/signals.py
+++ core/signals.py
@@ -1,4 +1,6 @@
+import datetime
 import os
+import pytz
 
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
@@ -31,6 +33,7 @@ def posh_user_saved(sender, instance, *args, **kwargs):
     if not instance.is_active:
         for listed_item in listed_items:
             if listed_item.status not in (ListedItem.NOT_LISTED, ListedItem.SOLD, ListedItem.REMOVED):
+                listed_item.datetime_removed = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
                 listed_item.status = ListedItem.REMOVED
                 listed_item.save()
 
