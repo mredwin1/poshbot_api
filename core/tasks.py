96d46c08113d30c382a5fa82ac694c0de3d592be
Edwin Cruz <ecruz1113@gmail.com>
2023-04-08 18:11:51 -0400

    Remove send SIGKILL

--- core/tasks.py
+++ core/tasks.py
@@ -639,15 +639,15 @@ def get_available_device(excluded_device_ids, logger):
             if device.is_ready():
                 return device
 
-        if device.checkout_time is not None and (timezone.now() - device.checkout_time).total_seconds() > 1200 and device.in_use:
-            campaign = Campaign.objects.get(posh_user__username=device.in_use)
-            if not campaign.sigkill_sent:
-                logger.info('Killing the campaign that is using this Device')
-                campaign.sigkill_sent = True
-                campaign.save(update_fields=['sigkill_sent'])
-                KillCampaignTask.delay(campaign.id)
-            else:
-                logger.info('SIGKILL was sent, campaign should terminate shortly')
+        # if device.checkout_time is not None and (timezone.now() - device.checkout_time).total_seconds() > 1200 and device.in_use:
+        #     campaign = Campaign.objects.get(posh_user__username=device.in_use)
+        #     if not campaign.sigkill_sent:
+        #         logger.info('Killing the campaign that is using this Device')
+        #         campaign.sigkill_sent = True
+        #         campaign.save(update_fields=['sigkill_sent'])
+        #         KillCampaignTask.delay(campaign.id)
+        #     else:
+        #         logger.info('SIGKILL was sent, campaign should terminate shortly')
 
 
 @shared_task
