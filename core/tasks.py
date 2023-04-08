472d489f554ade2ae9ff330574f1f92703264905
Edwin Cruz <ecruz1113@gmail.com>
2023-04-08 16:54:50 -0400

    Kill task if it has gone over 20min

--- core/tasks.py
+++ core/tasks.py
@@ -637,16 +637,11 @@ def get_available_device(excluded_device_ids, logger):
         if device.ip_reset_url not in in_use_ip_reset_urls:
             if device.is_ready():
                 return device
-        else:
-            if device.checkout_time is not None and (timezone.now() - device.checkout_time).total_seconds() > 1200:
-                logger.info('Another campaign will be started on this device because this one took too long.')
-                device.check_in()
 
-                in_use_ip_reset_urls = Device.objects.exclude(in_use='').values_list('ip_reset_url', flat=True)
-
-                if device.ip_reset_url not in in_use_ip_reset_urls:
-                    if device.is_ready():
-                        return device
+        if device.checkout_time is not None and (timezone.now() - device.checkout_time).total_seconds() > 1200 and device.in_use:
+            logger.info('Killing the campaign that is using this Device')
+            campaign = Campaign.objects.get(posh_user__username=device.in_use)
+            KillCampaignTask.delay(campaign.id)
 
 
 @shared_task
