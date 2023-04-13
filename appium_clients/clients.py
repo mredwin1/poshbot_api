39890f833db7ad7011b1733d52da4b95f4300880
Edwin Cruz <ecruz1113@gmail.com>
2023-04-13 11:08:56 -0400

    More logging

--- appium_clients/clients.py
+++ appium_clients/clients.py
@@ -396,9 +396,10 @@ class PoshMarkClient(AppiumClient):
             else:
                 element.send_keys(text)
 
-    def scroll_until_found(self, by, locator, swipe_direction='down', max_scrolls=10):
-        swipe_direction = 'up' if swipe_direction == 'down' else 'down'
+    def scroll_until_found(self, by, locator, direction='down', max_scrolls=10):
+        swipe_direction = 'up' if direction == 'down' else 'down'
         scroll_attempts = 0
+        self.logger.debug(f'Direction: {direction} Swipe Direction: {swipe_direction}')
         while not self.is_present(by, locator) and scroll_attempts < max_scrolls:
             self.logger.info(f'Could not find {locator}, scrolling...')
             self.swipe(swipe_direction, 1000)
