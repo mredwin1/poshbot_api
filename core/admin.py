0e18385b81f96dbc22f9e36d09fe9681c5c87979
Edwin Cruz <ecruz1113@gmail.com>
2023-04-15 12:47:25 -0400

    Remove the zipcode implementation

--- core/admin.py
+++ core/admin.py
@@ -169,7 +169,7 @@ class PoshUserAdmin(admin.ModelAdmin):
         ('Other Information', {
             'classes': ('collapse',),
             'fields': (
-                ('first_name', 'last_name', 'zipcode'),
+                ('first_name', 'last_name'),
                 ('date_of_birth', 'gender'),
                 ('profile_picture', 'profile_picture_id'),
                 ('header_picture')
