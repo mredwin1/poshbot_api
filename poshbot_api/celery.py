b6e8e069783a93567b7ac53aebecc7d927b55760
Edwin Cruz <ecruz1113@gmail.com>
2023-05-19 09:51:36 -0400

    Register the beat_scheduler

--- poshbot_api/celery.py
+++ poshbot_api/celery.py
@@ -1,8 +1,10 @@
 import os
+
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poshbot_api.settings.dev')
 
 app = Celery('poshbot_api')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.beat_scheduler = 'core.tasks.DedupScheduler'
 app.autodiscover_tasks()
