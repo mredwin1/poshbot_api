5d457e23ca7959208a33116cff4a32a48a63e4f4
Edwin Cruz <ecruz1113@gmail.com>
2023-09-01 13:39:19 -0400

    Changer url reverse

--- core/admin.py
+++ core/admin.py
@@ -116,9 +116,9 @@ class ListedItemReportAdmin(admin.ModelAdmin):
     list_display = ['posh_user', 'associated_listed_item_to_report', 'report_type', 'datetime_reported']
 
     @admin.display(ordering='listed_item_to_report')
-    def associated_listed_item_to_report(self, listed_item_to_report: models.ListedItemToReport):
-        url = f"{reverse('admin:core_listed_item_to_report_changelist')}?{urlencode({'id': str(listed_item_to_report.id)})}"
-        return format_html('<a href="{}">{}</a>', url, listed_item_to_report.listing_title)
+    def associated_listed_item_to_report(self, listed_item_report: models.ListedItemReport):
+        url = f"{reverse('admin:core_listed_item_to_report_changelist')}?{urlencode({'id': str(listed_item_report.listed_item_to_report.id)})}"
+        return format_html('<a href="{}">{}</a>', url, listed_item_report.listed_item_to_report.listing_title)
 
     @admin.display(ordering='listed_item_to_report__report_type')
     def report_type(self, obj):
