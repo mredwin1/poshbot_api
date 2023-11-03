eaba2d6d132ddb41a5aed9dcde26c59bec6759ac
Edwin Cruz <ecruz1113@gmail.com>
2023-11-02 22:08:22 -0400

    Force to use proxy for sharing for tobi and johnny

--- core/tasks.py
+++ core/tasks.py
@@ -767,10 +767,10 @@ class ManageCampaignsTask(Task):
                 campaign.next_runtime = None
 
                 campaign.save(update_fields=['status', 'queue_status', 'next_runtime'])
-            elif campaign.status == Campaign.IDLE and campaign.next_runtime is not None and (campaign.posh_user.user.username in ['tobi', 'johnny'] and available_proxy or campaign.posh_user.user.username not in ['tobi', 'johnny']):
+            elif campaign.status == Campaign.IDLE and campaign.next_runtime is not None and ((campaign.posh_user.user.username in ['tobi', 'johnny'] and available_proxy) or campaign.posh_user.user.username not in ['tobi', 'johnny']):
                 self.logger.info(f'Started as IDLE: {campaign.status} and runtime: {campaign.next_runtime} PROXY: {available_proxy} DEVICE: {available_device}')
                 campaign_started = self.start_campaign(campaign, available_device, available_proxy)
-            elif campaign.status == Campaign.STARTING and ((available_proxy and available_device) or (not need_to_list and campaign.posh_user.is_registered)):
+            elif campaign.status == Campaign.STARTING and ((available_proxy and available_device) or (not need_to_list and campaign.posh_user.is_registered and campaign.posh_user.user.username in ['tobi', 'johnny'])):
                 self.logger.info(f'Started as STARTING: {campaign.status} and runtime: {campaign.next_runtime} PROXY: {available_proxy} DEVICE: {available_device}')
                 campaign_started = self.start_campaign(campaign, available_device, available_proxy)
 
