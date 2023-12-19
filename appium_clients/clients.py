61ff9a9212d63f06521ce7de76c1d528cf1f2336
mredwin <ecruz1113@gmail.com>
2023-12-19 00:09:40 -0500

    Stop passing systemPort and mjpegServerPort

--- appium_clients/clients.py
+++ appium_clients/clients.py
@@ -53,8 +53,8 @@ class AppiumClient:
 
         capabilities["udid"] = device_serial
         capabilities["adbExecTimeout"] = 50000
-        capabilities["systemPort"] = system_port
-        capabilities["mjpegServerPort"] = mjpeg_server_port
+        # capabilities["systemPort"] = system_port
+        # capabilities["mjpegServerPort"] = mjpeg_server_port
 
         self.capabilities = capabilities
         self.capabilities_options = UiAutomator2Options().load_capabilities(
