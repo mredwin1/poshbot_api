2988d78e497b90ce13358ceb33e492292b1a7a17
mredwin <ecruz1113@gmail.com>
2023-12-26 14:24:07 -0500

    Make listing prices integers

--- core/models.py
+++ core/models.py
@@ -624,9 +624,9 @@ class Listing(models.Model):
     )
     description = models.TextField()
 
-    original_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    listing_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    lowest_price = models.DecimalField(max_digits=10, decimal_places=2, default=250)
+    original_price = models.PositiveSmallIntegerField(default=0)
+    listing_price = models.PositiveSmallIntegerField(default=0)
+    lowest_price = models.PositiveSmallIntegerField(default=250)
 
     campaign = models.ForeignKey(
         Campaign,
